8597607c3d8ad7dcb8f28ae217ff95992b63c4b0
emran <emranhi001@gmail.com>
2024-09-18 20:39:21 +0300

    started working on subscription module to handle users subscription to premium plans

--- stripe-project/src/modules/subscription/subscription.controller.ts
+++ stripe-project/src/modules/subscription/subscription.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { SubscriptionService } from './subscription.service';
+import Stripe from 'stripe';
 
 @Controller('subscription')
-export class SubscriptionController {}
+export class SubscriptionController {
+  constructor(private subscriptionService: SubscriptionService) {}
+
+  @Post()
+  createSubscriptionSession(
+    @Req() request,
+    @Body() priceId: string,
+  ): Promise<Stripe.Response<Stripe.Checkout.Session> | undefined> {
+        return this.subscriptionService.createSubscriptionSession(
+        request.user,
+        priceId,
+);
+  }
+}
--- stripe-project/src/modules/subscription/subscription.module.ts
+++ stripe-project/src/modules/subscription/subscription.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { SubscriptionController } from './subscription.controller';
 import { SubscriptionService } from './subscription.service';
+import { StripeModule } from '@golevelup/nestjs-stripe';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    StripeModule.forRootAsync(StripeModule, {
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) =>
+        configService.get('STRIPE_CONFIG'),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [SubscriptionController],
-  providers: [SubscriptionService]
+  providers: [SubscriptionService],
 })
 export class SubscriptionModule {}
